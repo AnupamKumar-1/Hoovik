842a8936a0feb45e451e0d54ef46983f844a777c
AnupamKumar-1 <anupam9594.kumar@gmail.com>
2026-04-07 11:07:12 +0530

    Update backend server URL

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,7 @@
-export const IS_PROD = false;
+export const IS_PROD = true;
 
 export const SERVER = IS_PROD
-  ? "https://skymeetai-backend.onrender.com"
+  ? "https://skymeetai-production.up.railway.app"
   : "http://192.168.1.15:8000";
 
 export const TRANSCRIPTS_ENABLED = !IS_PROD;
