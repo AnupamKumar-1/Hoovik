cf9642037245eab47ed0109a148a8c57509842e1
AnupamKumar-1 <anupam9594.kumar@gmail.com>
2026-04-16 02:16:26 +0530

    fix: remove duplicate CORS headers from emotion pro

--- backend/src/app.js
+++ backend/src/app.js
@@ -46,11 +46,17 @@ const emotionProxy = createProxyMiddleware({
   onProxyReqWs(proxyReq) {
     proxyReq.setHeader("Origin", "https://skymeetai.onrender.com");
   },
-  onProxyRes(proxyRes) {
+  onProxyRes(proxyRes, req, res) {
     delete proxyRes.headers["access-control-allow-origin"];
     delete proxyRes.headers["access-control-allow-credentials"];
     delete proxyRes.headers["access-control-allow-methods"];
     delete proxyRes.headers["access-control-allow-headers"];
+
+    Object.keys(proxyRes.headers).forEach((key) => {
+      res.setHeader(key, proxyRes.headers[key]);
+    });
+
+    proxyRes.pipe(res);
   },
 });
 
